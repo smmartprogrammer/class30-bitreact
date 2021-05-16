b7c8e4745bdc3df012275a26d4189fd0a89a53e1
Raghunathji <partabrajp@gmail.com>
2021-05-16 19:52:00 +0500

    update the git

---
+++ dist/src/components/header/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Header = function Header() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    children: "hello from header"
+  });
+};
+
+var _default = Header;
+exports.default = _default;
+
+//# sourceMappingURL=index.js.map
